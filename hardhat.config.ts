3071f924c0dcde287ce9e3a051969b25f8c8018c
chriss1525 <okothchris15@gmail.com>
2025-02-16 21:09:17 +0300

    feat: Write tokeAuthority logic to interact with the smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,20 +4,37 @@ import "dotenv/config";
 import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
+  },
   namedAccounts: {
     deployer: {
       default: 0,
     },
   },
   networks: {
-    arbitrumSepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}` || "",
-      accounts:
-        process.env.PRIVATE_KEY_ARBITRUM !== undefined
-          ? [process.env.PRIVATE_KEY_ARBITRUM]
-          : [],
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY_SEPOLIA}`],
     },
+    'sapphire-testnet': {
+      url: `https://testnet.sapphire.oasis.io`,
+      chainId: 23295,
+      accounts: [`0x${process.env.PRIVATE_KEY_OASIS}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    enabled: true,
   },
 };
 
